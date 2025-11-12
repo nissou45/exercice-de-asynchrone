c4441baf92c262b184b37421a00f18d8d4c7c13e
nissou45 <niss91@icloud.com>
2025-11-12 11:32:25 +0100

    Ajout de la partie utilisateurs avec fetch et pagination

---
+++ asynchrone.js
@@ -0,0 +1,6 @@
+//Liste d'utilisateurs : https://jsonplaceholder.typicode.com/users
+//Membre 1 - Récupérer et afficher les utilisateurs (5 utilisateurs avec pagination) :
+
+//Utilisez l'API https://jsonplaceholder.typicode.com/users.
+// Affichez les 5 premiers utilisateurs sur la page.
+// Ajoutez des boutons de pagination pour charger les utilisateurs suivants (affichage de 5 utilisateurs par page). Lorsqu'on clique sur le bouton "Suivant", les 5 utilisateurs suivants doivent être chargés.
